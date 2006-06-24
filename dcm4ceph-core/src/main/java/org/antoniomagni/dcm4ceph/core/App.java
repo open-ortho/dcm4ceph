a2eab031a370ae50575b31fba0f3b1be2ff1717f
tinton <tinton>
2006-06-24 21:43:27 +0000

    Initial directory structure of DICOM for Cephalograms.

---
+++ dcm4ceph-core/src/main/java/org/antoniomagni/dcm4ceph/core/App.java
@@ -0,0 +1,13 @@
+package org.antoniomagni.dcm4ceph.core;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ dcm4ceph-tool/dcm4ceph-tool-compliance/src/main/java/dcm4ceph/tool/compliance/App.java
@@ -0,0 +1,13 @@
+package dcm4ceph.tool.compliance;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
