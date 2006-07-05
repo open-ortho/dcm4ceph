1b3b1442e4ce8f340f89983716280793b1ac18c2
tinton <tinton>
2006-07-05 22:25:09 +0000

    Initial.

---
+++ dcm4ceph-ui/src/main/java/org/antoniomagni/dcm4ceph/ui/ceph2dicomdir/ceph2dcmdir.java
@@ -0,0 +1,41 @@
+/**
+ * dcm4ceph, a DICOM library for digital cephalograms
+ * Copyright (C) 2006  Antonio Magni
+ *
+ * This library is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ * 
+ * This library is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
+ * 
+ * Antonio Magni 
+ * email: dcm4ceph@antoniomagni.org
+ * website: http://dcm4ceph.antoniomagni.org
+ * 
+ */
+
+package org.antoniomagni.dcm4ceph.ui.ceph2dicomdir;
+
+/**
+ * @author afm
+ *
+ */
+public class ceph2dcmdir {
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        // TODO Auto-generated method stub
+
+    }
+
+}
