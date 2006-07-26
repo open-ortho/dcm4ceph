c7e90c6dbb55c4e95eb6001d4c3a214baf44c1d0
tinton <tinton>
2006-07-26 11:39:47 +0000

    worked on site.

---
+++ dcm4ceph-core/src/test/java/org/antoniomagni/dcm4ceph/core/LocaleTest.java
@@ -0,0 +1,19 @@
+package org.antoniomagni.dcm4ceph.core;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Locale;
+
+public class LocaleTest {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+//		List<Locale> locales = Arrays.asList(Locale.getAvailableLocales());
+//		for (int i = 0; i < locales.size(); i++) {
+//			System.out.println(locales.get(i).getDisplayName());
+//		}
+	}
+}
