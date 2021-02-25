1e5b0b910d840d04fe134f6c1d3951f273bb2309
Toni Magni <amagni@topsortho.com>
2021-02-25 11:05:04 -0500

    Remove trailing white spaces.

--- dcm4ceph-tool/dcm4ceph-tool-ceph2dicomdir/src/main/java/org/antoniomagni/dcm4ceph/tool/ceph2dicomdir/Ceph2DCM.java
+++ dcm4ceph-tool/dcm4ceph-tool-ceph2dicomdir/src/main/java/org/antoniomagni/dcm4ceph/tool/ceph2dicomdir/Ceph2DCM.java
@@ -6,20 +6,20 @@
  * modify it under the terms of the GNU Lesser General Public
  * License as published by the Free Software Foundation; either
  * version 2.1 of the License, or (at your option) any later version.
- * 
+ *
  * This library is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
  * Lesser General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Lesser General Public
  * License along with this library; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- * 
- * Antonio Magni 
+ *
+ * Antonio Magni
  * email: dcm4ceph@antoniomagni.org
  * website: http://dcm4ceph.antoniomagni.org
- * 
+ *
  */
 
 package org.antoniomagni.dcm4ceph.tool.ceph2dicomdir;
@@ -30,7 +30,7 @@ import org.antoniomagni.dcm4ceph.core.Cephalogram;
 
 /**
  * @author Antonio Magni <dcm4ceph@antoniomagni.org>
- * 
+ *
  */
 public class Ceph2DCM {
 
