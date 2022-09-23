6642a51850a4bbd22f7e47afd883b82d1254db65
Toni Magni <email@antoniomagni.com>
2022-09-23 12:29:15 -0400

    Update License to MIT.

--- dcm4ceph-core/src/main/java/org/open_ortho/dcm4ceph/core/Cephalogram.java
+++ dcm4ceph-core/src/main/java/org/open_ortho/dcm4ceph/core/Cephalogram.java
@@ -2,20 +2,6 @@
  * dcm4ceph, a DICOM library for digital cephalograms
  * Copyright (C) 2006  Toni Magni
  *
- * This library is free software; you can redistribute it and/or
- * modify it under the terms of the GNU Lesser General Public
- * License as published by the Free Software Foundation; either
- * version 2.1 of the License, or (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
- * Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public
- * License along with this library; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
- *
  * Toni Magni
  * email: afm@case.edu
  * website: https://github.com/open-ortho/dcm4ceph
